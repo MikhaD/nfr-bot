5be1a679605b3b0c62c6230ceb66aa5e427d6459
Mikha <mikhadavids@gmail.com>
2021-08-21 15:22:55 +0200

    added basic embed extension classes

--- src/utility/Embed.js
+++ src/utility/Embed.js
@@ -2,6 +2,11 @@ const { MessageEmbed, Collection } = require("discord.js");
 const config = require("../config.json");
 
 module.exports = class Embed extends MessageEmbed {
+	/**
+	 * Multi page supporting embed object
+	 * @param {String} title The title of the embed
+	 * @param {String} description The description of the embed
+	 */
 	constructor(title, description) {
 		super();
 
@@ -21,8 +26,21 @@ module.exports = class Embed extends MessageEmbed {
 		return this.pages.get(name);
 	}
 
+	/**
+	 * Add a page to this embed
+	 * @param {String} name The internal name for this page
+	 * @param {MessageEmbed} embed The embed to use as this page
+	 */
 	addPage(name, embed) {
 		this.pageNames.push(name);
 		this.pages.set(name, embed);
+	}
+
+	/**
+	 * A MessageOptions object containing this embed
+	 * @param {Boolean} buttons Add next page buttons if there are multiple pages
+	 */
+	message(buttons) {
+		return {embeds: [this]};
 	}
 };
