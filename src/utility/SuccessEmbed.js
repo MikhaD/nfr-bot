c42bd83e8528603fa9662493651c4d11b0bcc961
Mikha <mikhadavids@gmail.com>
2021-08-21 13:15:28 +0200

    added basic embed extension classes

---
+++ src/utility/SuccessEmbed.js
@@ -0,0 +1,12 @@
+const { MessageEmbed } = require("discord.js");
+const config = require("../config.json");
+
+module.exports = class Embed extends MessageEmbed {
+	constructor(title, description) {
+		super();
+
+		this.setTitle(`${title}`);
+		this.setDescription(`${description}`);
+		this.setColor(config.colors.embed.success);
+	}
+};
