6b038ddfcc327dcb840f1b4d77e7c7fdf3b8dbfd
Mikha <mikhadavids@gmail.com>
2021-10-24 23:47:15 +0200

    added a /lore command

---
+++ src/commands/guild/lore.js
@@ -0,0 +1,34 @@
+const { Embed } = require("../../utility/Embed");
+const MessageObject = require("../../utility/MessageObject");
+
+const pages = [
+	"https://media.discordapp.net/attachments/844148161647476756/901946411325739058/cover.png",
+	"https://media.discordapp.net/attachments/844148161647476756/901946418078576680/page1.png",
+	"https://media.discordapp.net/attachments/844148161647476756/901946421513703454/page2.png",
+	"https://media.discordapp.net/attachments/844148161647476756/901946420309921832/page3.png",
+	"https://media.discordapp.net/attachments/844148161647476756/901946426697850941/page4.png",
+	"https://media.discordapp.net/attachments/844148161647476756/901946425389236304/page5.png",
+	"https://media.discordapp.net/attachments/844148161647476756/901946429390598174/page6.png",
+	"https://media.discordapp.net/attachments/844148161647476756/901946433429700619/page7.png",
+	"https://media.discordapp.net/attachments/844148161647476756/901946469983092808/page8.png",
+	"https://media.discordapp.net/attachments/844148161647476756/901946477356650496/page9.png",
+	"https://media.discordapp.net/attachments/844148161647476756/901946481802641428/page10.png"
+];
+
+module.exports = {
+	name: "lore",
+	description: "The story of how NFR came to be, written by Fate, edited by Hiro & illustrated by MrRussetPotato",
+	cooldown: 60,
+	server: "739428526431666237",
+
+	async execute(interaction) {
+		const message = new MessageObject();
+		for (let i = 0; i < pages.length; ++i) {
+			const embed = new Embed();
+			embed.setImage(pages[i]);
+			embed.setFooter(`page ${i+1} of ${pages.length}`);
+			message.addPage(embed);
+		}
+		message.watchMessage(await interaction.followUp(message));
+	}
+};
